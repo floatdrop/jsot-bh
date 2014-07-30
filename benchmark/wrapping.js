1d80ce06daf32ecc42ad6c5f074737f1b3dcd22f
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-07-30 14:38:50 +0600

    Added benchmark on wrapping

---
+++ benchmark/wrapping.js
@@ -0,0 +1,34 @@
+var Benchmark = require('benchmark');
+var suite = new Benchmark.Suite();
+var benchmarks = require('beautify-benchmark');
+
+console.log('Benchmarking block wrapping...');
+
+function matchers(bh) {
+    bh.match('block', function (ctx) {
+        return [
+            '<before/>',
+            ctx.json(),
+            '<after/>'
+        ];
+    });
+}
+
+var JSOTBH = require('..');
+var jsotbh = new JSOTBH();
+matchers(jsotbh);
+
+var BH = require('bh').BH;
+var bh = new BH();
+matchers(bh);
+
+suite
+.add('jsotbh#block', function() {
+    jsotbh.apply({ block: 'block' });
+})
+.add('bh#block', function() {
+    bh.apply({ block: 'block' });
+})
+.on('cycle', function(event) { benchmarks.add(event.target); })
+.on('complete', function() { benchmarks.log(); })
+.run({ 'async': true });
